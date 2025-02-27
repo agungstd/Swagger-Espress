a1efdd7ccf5827f9fd34a7e17ca2036048a8fc2d
agungstd <apakatadunia11@gmail.com>
2025-02-27 14:10:11 +0700

    Simplify user data functions

--- src/users.js
+++ src/users.js
@@ -1,19 +1,14 @@
 let users = [
-  {
-    id: 1,
-    name: "user 1",
-    email: "exampleuser@email.com",
-  },
-  {
-    id: 2,
-    name: "user 2",
-    email: "exampleuser2@email.com",
-  },
-  {
-    id: 3,
-    name: "user 3",
-    email: "exampleuser3@email.com",
-  },
+  { id: 1, name: "user 1", email: "exampleuser@email.com" },
+  { id: 2, name: "user 2", email: "exampleuser2@email.com" },
+  { id: 3, name: "user 3", email: "exampleuser3@email.com" },
+  { id: 4, name: "user 4", email: "exampleuser4@email.com" },
+  { id: 5, name: "user 5", email: "exampleuser5@email.com" },
+  { id: 6, name: "user 6", email: "exampleuser6@email.com" },
+  { id: 7, name: "user 7", email: "exampleuser7@email.com" },
+  { id: 8, name: "user 8", email: "exampleuser8@email.com" },
+  { id: 9, name: "user 9", email: "exampleuser9@email.com" },
+  { id: 10, name: "user 10", email: "exampleuser10@email.com" }
 ];
 
 export const getAll = (req, res) => {
